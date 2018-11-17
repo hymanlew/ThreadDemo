e941e0865ae38518cd82e56dd17fdd620c688c35
hyman <hyman@163.com>
2018-11-17 20:40:11 +0800

    after merge - ok

--- src/main/java/LockAbout/LockDemo2.java
+++ src/main/java/LockAbout/LockDemo2.java
@@ -1,5 +1,11 @@
 package LockAbout;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.concurrent.*;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * Java里面内置锁 (synchronized) 和 Lock(ReentrantLock) 都是可重入的。Reentrant /rintrent/：可重入的。
  *
@@ -13,9 +19,9 @@ package LockAbout;
  *
  * 4）通过Lock可以知道有没有成功获取锁，而synchronized却无法办到。
  *
- * 5）Lock可以提高多个线程进行读操作的效率。
+ * 5）Lock可以提高多个线程进行读操作的效率，具有嗅探锁定，多路分支通知等功能，而且使用上也更加灵活。
  *
- * 总结：ReentrantLock相比synchronized，增加了一些高级的功能。但也有一定缺陷。在ReentrantLock类中定义了很多方法，比如：
+ * 总结：ReentrantLock相比synchronized，增加了一些高级的功能。但也有一定缺陷。在ReentrantLock类中定义了很多方法（在 tn 方法中），比如：
  * isFair()        //判断锁是否是公平锁
  * isLocked()    //判断锁是否被任何线程获取了
  * isHeldByCurrentThread()   //判断锁是否被当前线程获取了
@@ -28,8 +34,8 @@ package LockAbout;
  * 的线程中中断它，这种就是可中断锁。
  * lockInterruptibly()的用法体现了Lock的可中断性。
  *
- * 2) 公平锁，即尽量以请求锁的顺序来获取锁。比如同是有多个线程在等待一个锁，当这个锁被释放时，等待时间最久的线程（最先请求的线程）
- * 会获得该锁（并不是绝对的，大体上是这种顺序），这种就是公平锁。
+ * 2) 公平锁，即尽量以请求锁的顺序来获取锁。线程获取锁的顺序是按照线程加锁的顺序来分配的，即 FIFO（first in first out）规则。
+ * 这种就是公平锁。
  * 非公平锁即无法保证锁的获取是按照请求锁的顺序进行的。这样就可能导致某个或者一些线程永远获取不到锁。
  * 在Java中，synchronized就是非公平锁，它无法保证等待的线程获取锁的顺序。ReentrantLock可以设置成公平锁。
  *
@@ -38,7 +44,7 @@ package LockAbout;
  * 它的资源。那么如何解决饥饿问题呢，这就需要公平锁了。
  *
  * 公平锁可以保证线程按照时间的先后顺序执行，避免饥饿现象的产生。但公平锁的效率比较低，因为要实现顺序执行，需要维护一个有序队列。
- * ReentrantLock 便是一种公平锁，通过在构造方法中传入true就是公平锁，传入false，就是非公平锁。
+ * ReentrantLock 便是一种公平锁机制，通过在构造方法中传入true就是公平锁，传入false，就是非公平锁。默认是非公平锁。
  *
  * 3) 读写锁，读写锁将对一个资源（比如文件）的访问分成了2个锁，一个读锁和一个写锁。正因为有了读写锁，才使得多个线程之间的读操作可
  * 以并发进行，不需要同步，而写操作需要同步进行，提高了效率。
@@ -60,4 +66,441 @@ package LockAbout;
  * 下，优先考虑使用synchronized来进行同步。
  */
 public class LockDemo2 {
+
+    public static ThreadPoolExecutor pool = new ThreadPoolExecutor(5,5,0, TimeUnit.SECONDS,new LinkedBlockingQueue<>());
+
+    public static void t1(){
+        // getHolddCount() == 查询当前线程保持锁定的个数，即调用 lock 方法的次数
+        Tservice tservice = new Tservice();
+        tservice.method();
+        tservice.method2();
+    }
+
+    public static void t2(){
+        // getQueueLength() == 查询正等待获取此锁定的线程估计数。如果有 5个线程，1个线程先执行 await 方法，那么调用此方法则
+        // 返回值是 4，说明有 4个线程同时在等待 lock 的释放。
+        Tservice tservice = new Tservice();
+
+        for(int i=0; i<pool.getCorePoolSize(); i++){
+            pool.execute(new Runnable() {
+                @Override
+                public void run() {
+                    tservice.method3();
+                }
+            });
+        }
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("现有 = "+tservice.lock.getQueueLength()+"个线程在等待获取锁！");
+    }
+
+    public static void t3(){
+        // getWaitQueueLength(condition) == 返回等待与此锁定相关的给定条件 conditon 的线程估计数。比如有 5个线程，每个线程都
+        // 执行了一个 conditon 对象的 await 方法，那么调用此方法则返回值是 5，说明有 5个线程同时在等待 lock 的释放。
+        Tservice tservice = new Tservice();
+
+        for(int i=0; i<pool.getCorePoolSize(); i++){
+            pool.execute(new Runnable() {
+                @Override
+                public void run() {
+                    tservice.waitm();
+                }
+            });
+        }
+        try {
+            Thread.sleep(2000);
+            tservice.notifytm();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void t4(){
+        // hasQueuedThread(thread) == 判断指定的线程是否有在等待该锁。
+        // hasQueuedThreads() == 判断是否有线程在等待该锁。
+        Tservice tservice = new Tservice();
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                // 在此案例中，不能使用 condition 的 await 方法。因为它是使用的 wait 方法即自动释放锁
+                //tservice.waitm();
+                tservice.method3();
+            }
+        };
+
+        try {
+            Thread a = new Thread(runnable);
+            a.start();
+            Thread.sleep(500);
+            Thread b = new Thread(runnable);
+            b.start();
+            Thread.sleep(500);
+
+            System.out.println("a 线程是否在等待获取锁 = "+tservice.lock.hasQueuedThread(a));
+            System.out.println("b 线程是否在等待获取锁 = "+tservice.lock.hasQueuedThread(b));
+            System.out.println("是否有线程在等待获取锁 = "+tservice.lock.hasQueuedThreads());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void t5(){
+        // hasWaiters(condition) == 判断是否有线程正在等待与该锁有关的 condition 条件。
+        Tservice tservice = new Tservice();
+
+        for(int i=0; i<pool.getCorePoolSize(); i++){
+            pool.execute(new Runnable() {
+                @Override
+                public void run() {
+                    tservice.waitm();
+                }
+            });
+        }
+        try {
+            Thread.sleep(2000);
+            tservice.checkWait();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void t6(){
+        // isHeldByCurrentThread() == 判断锁是否被当前线程获取了
+        // isLocked() == 判断该锁是否由任意线程保持
+        Tservice tservice = new Tservice(true);
+        pool.execute(new Runnable() {
+            @Override
+            public void run() {
+                tservice.method4();
+            }
+        });
+    }
+
+    public static void t7(){
+        Tservice tservice = new Tservice();
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                tservice.method5();
+            }
+        };
+        try {
+            // 由于两个线程调用的都是同一个对象的方法，因为 lock 锁而产生同步关系。所以会先执行完 a，再执行 b。
+            Thread a = new Thread(runnable);
+            a.setName("a");
+            a.start();
+            Thread.sleep(500);
+            Thread b = new Thread(runnable);
+            b.setName("b");
+            b.start();
+            b.interrupt();
+
+            System.out.println("main end = ");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void t8(){
+        Tservice tservice = new Tservice(true);
+        ThreadFactory names = new ThreadFactory() {
+            private int i = 0;
+            @Override
+            public Thread newThread(Runnable r) {
+                return new Thread(r,"线程-"+(i++));
+            }
+        };
+        pool = new ThreadPoolExecutor(2,2,0,TimeUnit.SECONDS,new LinkedBlockingQueue<>(),names);
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                tservice.method6();
+            }
+        };
+        pool.execute(runnable);
+        pool.execute(runnable);
+    }
+
+    public static void t9(){
+        Tservice tservice = new Tservice();
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                tservice.method7();
+            }
+        };
+        try {
+            Thread thread = new Thread(runnable);
+            thread.start();
+            Thread.sleep(3000);
+            thread.interrupt();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void t10(){
+        Tservice tservice = new Tservice();
+        Runnable wait = new Runnable() {
+            @Override
+            public void run() {
+                tservice.wait2();
+            }
+        };
+        Runnable notify = new Runnable() {
+            @Override
+            public void run() {
+                tservice.notify2();
+            }
+        };
+        try {
+            pool.execute(wait);
+            Thread.sleep(2000);
+            pool.execute(notify);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        // getHolddCount() == 查询当前线程保持锁定的个数，即调用 lock 方法的次数
+        //t1();
+        // getQueueLength() == 查询正等待获取此锁定的线程估计数。
+        //t2();
+        // getWaitQueueLength(condition) == 返回等待与此锁定相关的给定条件 conditon 的线程估计数。
+        //t3();
+        // hasQueuedThread(thread) == 判断指定的线程是否有在等待该锁。
+        //t4();
+        // hasWaiters(condition) == 判断是否有线程正在等待与该锁有关的 condition 条件。
+        //t5();
+        // isHeldByCurrentThread() == 判断锁是否被当前线程获取了
+        //t6();
+        // lockInterruptibly() == 如果当前线程未被中断，则获取锁定。如果已被中断则出现异常。
+        //t7();
+        // trylock() == 仅在调用时检查该锁对象未被另一个线程锁定的情况下，才获取锁定对象加锁。
+        //t8();
+        // awaitUninterruptibly() == 该方法强制当前线程忽略中断命令，而是继续正常执行。
+        //t9();
+        // awaitUntil() == 使得当前线程阻塞指定的时间之后（从当前时间开始算），再继续执行。并且它允许被其他线程提前唤醒。
+        t10();
+
+        pool.shutdown();
+    }
+}
+
+class Tservice {
+    public ReentrantLock lock = new ReentrantLock();
+    public Tservice(){
+
+    }
+
+    public void method(){
+        try {
+            lock.lock();
+            System.out.println(Thread.currentThread().getName()+" hold count = "+lock.getHoldCount());
+            method2();
+        }finally {
+            lock.unlock();
+        }
+    }
+
+    public void method2(){
+        try {
+            lock.lock();
+            System.out.println(Thread.currentThread().getName()+" hold count = "+lock.getHoldCount());
+        }finally {
+            lock.unlock();
+        }
+    }
+
+    public void method3(){
+        try {
+            lock.lock();
+            System.out.println(Thread.currentThread().getName()+" hold ==");
+            Thread.sleep(8000);
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            lock.unlock();
+        }
+    }
+
+
+    private Condition condition = lock.newCondition();
+    public void waitm(){
+        try {
+            lock.lock();
+            condition.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }finally {
+            lock.unlock();
+        }
+    }
+
+    public void notifytm(){
+        try {
+            lock.lock();
+            System.out.println("现有 = "+lock.getWaitQueueLength(condition)+"个线程在等待 condition 获取锁！");
+            condition.signal();
+        }finally {
+            lock.unlock();
+        }
+    }
+
+    public void checkWait(){
+        try {
+            lock.lock();
+            System.out.println("是否有线程在等待 condition 获取锁 = "+lock.hasWaiters(condition)+"，线程数量为 = "+lock.getWaitQueueLength(condition));
+            condition.signal();
+        }finally {
+            lock.unlock();
+        }
+    }
+
+
+    // ReentrantLock 便是一种公平锁机制，通过在构造方法中传入true就是公平锁，传入false，就是非公平锁。默认是非公平锁。
+    public Tservice(boolean isFair){
+        lock = new ReentrantLock(isFair);
+    }
+    public void method4(){
+        System.out.println("是否是公平锁 == "+lock.isFair());
+        try {
+            System.out.println("是否由任意线程保持1 = "+lock.isLocked());
+            System.out.println("是否获取锁1 = "+lock.isHeldByCurrentThread());
+            lock.lock();
+            System.out.println("是否获取锁2 = "+lock.isHeldByCurrentThread());
+            System.out.println("是否由任意线程保持2 = "+lock.isLocked());
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            lock.unlock();
+        }
+    }
+
+    public void method5(){
+        /**
+         * Lock() 方法可以让等待锁的线程响应中断，即当当前线程被 interrupt 后，它也不会抛出异常而是正常结束，最后释放锁。
+         * 而synchronized却不行，使用synchronized时，等待的线程会一直等待下去，不能够响应中断；
+         *
+         * lockInterruptibly() == 如果当前线程未被中断，则获取锁定。如果已被中断则出现异常。
+         */
+        try {
+            //lock.lock();
+            lock.lockInterruptibly();
+            System.out.println("lock begin = "+Thread.currentThread().getName());
+            // 不断的创建对象
+            for(int i=0; i<10000000; i++){
+                String s = new String();
+                Math.random();
+                //System.out.println("-----");
+            }
+            System.out.println("lock end = "+Thread.currentThread().getName());
+        }catch (Exception e){
+            e.printStackTrace();
+            // 只有自定义异常并输出信息时，才能 get 到 message。并且以下方式都不能得到想要的信息
+            System.out.println("exception1 == "+e.getMessage());
+            System.out.println("exception2 == "+e.getCause());
+            System.out.println("exception3 == "+e.getStackTrace());
+            System.out.println("exception4 == "+System.err);
+        }finally {
+            if(lock.isHeldByCurrentThread()){
+                lock.unlock();
+            }
+        }
+    }
+
+    public void method6(){
+        // trylock() == 仅在调用时检查该锁对象未被另一个线程锁定的情况下，才获取锁定对象加锁。
+        if(lock.tryLock()){
+            System.out.println(Thread.currentThread().getName()+" = 获得锁！");
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }else{
+            System.out.println(Thread.currentThread().getName()+" = 没有获得锁！");
+        }
+        System.out.println(Thread.currentThread().getName()+" 是否锁定 = "+lock.isHeldByCurrentThread());
+        if(lock.isHeldByCurrentThread()){
+            lock.unlock();
+        }
+
+        // trylock(time，timeUnit) == 仅在调用时检查该锁对象在指定的时间内未被另一个线程锁定，且当前线程未被中断的情况下，
+        // 才获取锁定对象加锁。
+        try {
+            System.out.println("=== 等待锁释放 "+Thread.currentThread().getName()+" ===");
+            Thread.sleep(8000);
+
+            if(lock.tryLock(2,TimeUnit.SECONDS)){
+                System.out.println(Thread.currentThread().getName()+" = 超时获得锁 ==2");
+                Thread.sleep(5000);
+                System.out.println(Thread.currentThread().getName()+" = 运行结束！");
+            }else{
+                System.out.println(Thread.currentThread().getName()+" = 没有获得锁 ==2");
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }finally {
+            if(lock.isHeldByCurrentThread()){
+                lock.unlock();
+            }
+        }
+    }
+
+    public void method7(){
+        try {
+            lock.lock();
+            System.out.println("wait begin == ");
+
+            // 使用 await 方法时，如果当前线程被中断则会抛出异常。这时就要用 awaitUninterruptibly 方法
+            //condition.await();
+            condition.awaitUninterruptibly();
+            System.out.println("wait end == ");
+        }catch (Exception e){
+            e.printStackTrace();
+            System.out.println("catch exception ==");
+        }finally {
+            lock.unlock();
+        }
+    }
+
+    public void wait2(){
+        try {
+            Calendar calendar = Calendar.getInstance();
+            // 该方法是在当前日期上再加上指定的时间。
+            calendar.add(Calendar.SECOND,10);
+
+            lock.lock();
+            System.out.println("wait begin == "+System.currentTimeMillis());
+            System.out.println(new Date()+" == "+calendar.getTime());
+
+            // 该方法是使得当前线程阻塞指定的时间之后（从当前时间开始算），再继续执行。并且它允许被其他线程提前唤醒。
+            condition.awaitUntil(calendar.getTime());
+            System.out.println("wait end == "+System.currentTimeMillis());
+        }catch (Exception e){
+            e.printStackTrace();
+            System.out.println("catch exception ==");
+        }finally {
+            lock.unlock();
+        }
+    }
+    public void notify2(){
+        try {
+            Calendar calendar = Calendar.getInstance();
+            calendar.add(Calendar.SECOND,10);
+
+            lock.lock();
+            System.out.println("notify begin == "+System.currentTimeMillis());
+            condition.signalAll();
+            System.out.println("notify end == "+System.currentTimeMillis());
+        }finally {
+            lock.unlock();
+        }
+    }
 }
--- src/main/java/communication/ThreadPoolDemo.java
+++ src/main/java/communication/ThreadPoolDemo.java
@@ -6,18 +6,146 @@ import java.util.concurrent.*;
 /**
  * concurrent /kernkarent/：并发的。
  * executor /igzekiuter/：执行者。
+ *
+ *
+     int corePoolSize：该线程池中核心线程数最大值。
+     核心线程：线程池新建线程的时候，如果当前线程总数小于corePoolSize，则新建的是核心线程，如果超过corePoolSize，则新建
+     的是非核心线程。核心线程默认情况下会一直存活在线程池中，即使这个核心线程啥也不干(闲置状态)。
+     如果指定 ThreadPoolExecutor 的 allowCoreThreadTimeOut 这个属性为 true，那么核心线程如果不干活(闲置状态)的话，超过
+     一定时间(时长下面参数决定)，就会被销毁掉。
+
+     int maximumPoolSize： 该线程池中线程总数最大值。线程总数 = 核心线程数 + 非核心线程数。
+
+     long keepAliveTime：该线程池中非核心线程闲置超时时长。一个非核心线程，如果不干活(闲置状态)的时长超过这个参数所设定的
+     时长，就会被销毁掉，如果设置 allowCoreThreadTimeOut = true，则会作用于核心线程。
+
+     TimeUnit unit：keepAliveTime的单位，它是一个枚举类型，其包括：
+     NANOSECONDS ： 1微毫秒 = 1微秒 / 1000
+     MICROSECONDS ： 1微秒 = 1毫秒 / 1000
+     MILLISECONDS ： 1毫秒 = 1秒 /1000
+     SECONDS ： 秒
+     MINUTES ： 分
+     HOURS ： 小时
+     DAYS ： 天
+
+     BlockingQueue workQueue：该线程池中的任务队列：维护着等待执行的 Runnable 对象。当所有的核心线程都在干活时，新添加
+            的任务会被添加到这个队列中等待处理，如果队列满了，则新建非核心线程执行任务。常用的workQueue类型：
+
+     SynchronousQueue：这个队列接收到任务的时候，会直接提交给线程处理，而不保留它，如果所有线程都在工作怎么办？那就新建
+             一个线程来处理这个任务！所以为了保证不出现 ‘线程数达到了 maximumPoolSize 而不能新建线程’ 的错误，使用这个类
+             型队列的时候，maximumPoolSize一般指定成Integer.MAX_VALUE，即无限大。
+
+     LinkedBlockingQueue：即链表类型的阻塞队列。这个队列接收到任务的时候，如果当前线程数小于核心线程数，则新建线程(核心
+             线程)处理任务；如果当前线程数等于核心线程数，则进入队列等待。由于这个队列没有最大值限制，默认是 Integer.MAX_
+             VALUE，即无限大。即所有超过核心线程数的任务都将被添加到队列中，这也就导致了maximumPoolSize的设定失效，从而
+             可能就耗费内存非常大导致OOM。
+             也可以自定义队列任务量（使用构造方法传值），当队列满时，就创建新的线程，但如果超过了最大线程数就会抛异常。
+
+     ArrayBlockingQueue：可以限定队列的长度，接收到任务的时候，如果没有达到corePoolSize的值，则新建线程(核心线程)执行任
+             务，如果达到了则入队等候，如果队列已满，则新建线程(非核心线程)执行任务，又如果总线程数到了maximumPoolSize，并
+             且队列也满了，则发生错误。
+
+     DelayQueue：队列内元素必须实现Delayed接口，这就意味着你传进去的任务必须先实现Delayed接口。这个队列接收到任务时，首先
+            先入队，只有达到了指定的延时时间，才会执行任务。
+
+     ThreadFactory threadFactory：创建线程的方式，这是一个接口，new 的时候需要实现它的 Thread newThread(Runnable r)方
+            法，一般用不上。
+
+     RejectedExecutionHandler handler：它是抛出异常专用的，比如上面提到的两个错误发生了（线程数达到了 maxiSize），就会
+            由这个 handler 抛出异常，根本用不上。
+ *
+ *
+ * 在手动创建线程池时，要特别注意一点，一定要设置非核心线程闲置超时时长。否则非核心线程创建即销毁，没有线程接收任务就会抛异常。
+ *
+ *
+ * 说明：Executors各个方法的弊端：
+ * 1）newFixedThreadPool 和 newSingleThreadExecutor：核心线程数 = 最大线程数，但因为其默认队列大小是整数的最大值，所以如果
+ *    过多堆积的请求处理，队列可能会耗费非常大的内存，甚至OOM。。
+ * 2）newCachedThreadPool 和 newScheduledThreadPool：因为线程数最大数是Integer.MAX_VALUE，可能会创建数量非常多的线程，甚至OOM。
+ *
  */
 public class ThreadPoolDemo {
 
-    public static void t1(){
-        // 固定线程数量的线程池
+    public static void mainNote(){
+        /**
+         * 创建固定线程数量的线程池：
+         * 不能使用下面的这种方式，而是要手动去创建。因为 Executor 其方法的源码就是使用的 ThreadPoolExecutor。
+         * 线程池不允许使用Executors去创建，而是通过ThreadPoolExecutor的方式，这样的处理方式可以更加明确线程池的运行规则，规
+         * 避资源耗尽的风险。
+         *
+         * 而阿里提示的创建线程或线程池时需要指定线程名称，是过时的方法，已经被删除了。线程池会自己定义名称。
+         * 也可以重写 newThread 方法定义名称。
+         */
         ExecutorService pool = Executors.newFixedThreadPool(3);
 
+        ThreadFactory namefactory = Executors.defaultThreadFactory();
+
+        namefactory = new ThreadFactory() {
+            private int index = 0;
+            @Override
+            public Thread newThread(Runnable r) {
+                return new Thread(r, "线程-"+(index++));
+            }
+        };
+
+        ThreadPoolExecutor pooln = new ThreadPoolExecutor(3, 10,
+                5L, TimeUnit.SECONDS,
+                new LinkedBlockingQueue<Runnable>(10),namefactory,new ThreadPoolExecutor.AbortPolicy());
+
+        /**
+         * 创建线程数量动态变化的缓存线程池：
+         * 1，这种线程池内部没有核心线程，线程的数量是有没限制的。
+         * 2，在创建任务时，若有空闲的线程时则复用空闲的线程，若没有则新建线程。
+         * 3，没有工作的线程（闲置状态）在超过了60S还不做事，就会销毁。
+         */
+        ExecutorService pool2 = Executors.newCachedThreadPool();
+
+        for(int i=0; i<20; i++) {
+            pooln.execute(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        System.out.println(Thread.currentThread().getName() + " == ");
+                        //Thread.sleep(1000);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            System.out.println("核心线程数" + pooln.getCorePoolSize());
+            System.out.println("线程池数" + pooln.getPoolSize());
+            System.out.println("队列任务数" + pooln.getQueue().size());
+            System.out.println("================");
+        }
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("======== 最后 ========");
+        System.out.println("核心线程数" + pooln.getCorePoolSize());
+        System.out.println("线程池数" + pooln.getPoolSize());
+        System.out.println("队列任务数" + pooln.getQueue().size());
+
+        pooln.shutdown();
+    }
+
+    public static void t1(){
+        // 固定线程数量的线程池。
+        //ExecutorService pool = new ThreadPoolExecutor(3, 3,
+        //        0L, TimeUnit.MILLISECONDS,
+        //        new LinkedBlockingQueue<Runnable>());
+
         // 线程数量动态变化的缓存线程池
-        //ExecutorService pool = Executors.newCachedThreadPool();
+        //ExecutorService pool = new ThreadPoolExecutor(0, 100,
+        //        60L, TimeUnit.SECONDS,
+        //        new SynchronousQueue<Runnable>());
 
         // 单线程的线程池，其优点是始终会保持有一个活跃的线程在工作。即如果当前线程死掉，它就会重新创建一个新的线程来工作
-        //ExecutorService pool = Executors.newSingleThreadExecutor();
+        ExecutorService pool = new ThreadPoolExecutor(1, 1,
+                        0L, TimeUnit.MILLISECONDS,
+                        new LinkedBlockingQueue<Runnable>());
 
         for(int i=0; i<10; i++) {
             // 在子循环中调用私有变量，则此外部的私有变量需要声明为 final 的。
@@ -54,7 +182,9 @@ public class ThreadPoolDemo {
          * scheduleAtFixedRate（调用 runnable 对象执行任务，间隔时间，之后间隔的时间，时间单位），该方法会在指定的时间之后
          * 执行一次，然后再按照指定的时间间隔有频率的执行。
          */
-        Executors.newScheduledThreadPool(3).scheduleAtFixedRate(new Runnable() {
+        //ScheduledExecutorService service = Executors.newScheduledThreadPool(3);
+        ScheduledExecutorService service = new ScheduledThreadPoolExecutor(3);
+        service.scheduleAtFixedRate(new Runnable() {
             @Override
             public void run() {
                 System.out.println("定时器执行 ===");
@@ -116,10 +246,12 @@ public class ThreadPoolDemo {
 
     public static void main(String[] args) {
         // 三种线程池的使用
-        t1();
+        //t1();
         // 线程池的定时器
         //t2();
-        // 线程池的定时器
+        // 线程池的等待/通知模式功能
         //t3();
+        // 优化线程池的创建方式
+        mainNote();
     }
 }
